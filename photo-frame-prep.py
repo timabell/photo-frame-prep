27f564356caedfaabdfd071948fc284658e66a04
Tim Abell <tim.abell@timwise.co.uk>
2008-10-04 20:31:46 +0100

    subclass gui. button click event handler stub.

--- photo-frame-prep.py
+++ photo-frame-prep.py
@@ -4,7 +4,23 @@
 
 import wx #import normal wxPython widgets library
 import gui # import wxGlade generated gui from gui.py
+
+#inherit from the gui class
+
+class MyApp(gui.MyFrame):
+	def __init__(self, *args, **kwds):
+		gui.MyFrame.__init__(self, *args, **kwds) #call base class init
+		#bind event handler for go button
+		self.gobutton.Bind(wx.EVT_BUTTON,  self.goClicked)
+		
+	#event handler for go button
+	def goClicked(source,  event):
+		print 'clicked'
+
+
+print 'starting up app...'
 app = wx.App() # um, something to initialize the app i guess. don't really know
-frame = gui.MyFrame(None, -1,  'photo-frame-prep.py') # load the frame defined in our gui file
+frame = MyApp(None, -1,  'photo-frame-prep.py') # load the frame overriden gui class
 frame.Show() #show the frame (presumably)
 app.MainLoop() #run the app, or something
+
