2fc0a5cee0dda68be92f2e167bd3932ff067862b
Tim Abell <tim.abell@timwise.co.uk>
2008-10-04 19:23:09 +0100

    show my gui, instead of a blank one

--- photo-frame-prep.py
+++ photo-frame-prep.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python
 
-# http://www.zetcode.com/wxpython/firststeps/
+# http://www.zetcode.com/wxpython/firststeps/ - how to show a frame
 
-import wx
-app = wx.App()
-frame = wx.Frame(None, -1,  'photo-frame-prep.py')
-frame.Show()
-app.MainLoop()
+import wx #import normal wxPython widgets library
+import gui # import wxGlade generated gui from gui.py
+app = wx.App() # um, something to initialize the app i guess. don't really know
+frame = gui.MyFrame(None, -1,  'photo-frame-prep.py') # load the frame defined in our gui file
+frame.Show() #show the frame (presumably)
+app.MainLoop() #run the app, or something
