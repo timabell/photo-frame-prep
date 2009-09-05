a51624c64f28b144facba717a20259943a2cc3b4
Tim Abell <tim@timwise.co.uk>
2009-09-05 06:40:31 +0100

    build script update after deb folder moved

--- builddeb.sh
+++ builddeb.sh
@@ -4,16 +4,16 @@ version=$1 #get version from first arg
 packagename="photoframeprep_${version}_all.deb"
 packager="Tim Abell <tim@timwise.co.uk>"
 date=$(/bin/date)
-target="packaging/photoframeprep"
+target="debian/photoframeprep"
 rm -rf $target
 mkdir -p $target/DEBIAN/
 mkdir -p $target/usr/bin/
 mkdir -p $target/usr/share/doc/photoframeprep/
 mkdir -p $target/usr/share/pyshared/photoframeprep/
-sed s/#version#/$version/ packaging/control > $target/DEBIAN/control
-sed "s/#packager#/$packager/" packaging/copyright > $target/usr/share/doc/photoframeprep/copyright
+sed s/#version#/$version/ debian/control > $target/DEBIAN/control
+sed "s/#packager#/$packager/" debian/copyright > $target/usr/share/doc/photoframeprep/copyright
 sed -i "s/#date#/$date/"  $target/usr/share/doc/photoframeprep/copyright
-cd packaging/
+cd debian/
 gzip -c -9 changelog > ../$target/usr/share/doc/photoframeprep/changelog.gz
 cd ..
 cp photo-frame-prep.py $target/usr/bin/photo-frame-prep
