dd457112bb55e1b6450bf50d957f2cb181b48697
Tim Abell <tim@timwise.co.uk>
2009-09-05 05:19:53 +0100

    first attempt at .deb generation

---
+++ builddeb.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+version="0.1"
+packagename="photoframeprep_${version}_all.deb"
+mkdir -p packaging/photoframeprep/DEBIAN/
+mkdir -p packaging/photoframeprep/usr/bin/
+cp photo-frame-prep.py packaging/photoframeprep/usr/bin/photo-frame-prep
+cd packaging/
+sed s/#version#/$version/ control > photoframeprep/DEBIAN/control
+echo "fakeroot dpkg -b photoframeprep $packagename"
+fakeroot dpkg -b photoframeprep $packagename
+lintian $packagename
