f135a9c42bb7f6f312ec53379b22146c6e282d69
Tim Abell <tim@timwise.co.uk>
2009-09-05 06:34:36 +0100

    changelog added (not right yet)

--- builddeb.sh
+++ builddeb.sh
@@ -5,6 +5,7 @@ packagename="photoframeprep_${version}_all.deb"
 packager="Tim Abell <tim@timwise.co.uk>"
 date=$(/bin/date)
 target="packaging/photoframeprep"
+rm -rf $target
 mkdir -p $target/DEBIAN/
 mkdir -p $target/usr/bin/
 mkdir -p $target/usr/share/doc/photoframeprep/
@@ -12,6 +13,9 @@ mkdir -p $target/usr/share/pyshared/photoframeprep/
 sed s/#version#/$version/ packaging/control > $target/DEBIAN/control
 sed "s/#packager#/$packager/" packaging/copyright > $target/usr/share/doc/photoframeprep/copyright
 sed -i "s/#date#/$date/"  $target/usr/share/doc/photoframeprep/copyright
+cd packaging/
+gzip -c -9 changelog > ../$target/usr/share/doc/photoframeprep/changelog.gz
+cd ..
 cp photo-frame-prep.py $target/usr/bin/photo-frame-prep
 cp gui.py $target/usr/share/pyshared/photoframeprep/
 fakeroot dpkg -b $target $packagename
